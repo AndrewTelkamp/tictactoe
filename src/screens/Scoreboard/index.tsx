b4db1e1dd293fdcd9ecac2c5dada05b4e40dbc6a
andrewtelkamp <andrew.telkamp@gmail.com>
2023-02-05 22:13:13 -0700

    build scoreboard screen

--- src/screens/Scoreboard/index.tsx
+++ src/screens/Scoreboard/index.tsx
@@ -1,11 +1,81 @@
-import {Text} from 'react-native';
-import {Screen} from '../../components';
+import {Text, View} from 'react-native';
 
-function Scoreboard(props: any) {
+import {HeaderText, Screen} from '../../components';
+import {useGameStore} from '../../state';
+import styles from './styles';
+
+import type {MatchResult} from '../../state/Game';
+
+interface Ranking {
+  player: string;
+  wins: number;
+}
+
+function formatWins(games: MatchResult[]): Ranking[] {
+  const result = [];
+  const wins: any = {};
+
+  games.forEach(game => {
+    if (!wins[game.player]) {
+      wins[game.player] = 0;
+    }
+    wins[game.player] = wins[game.player] + 1;
+  });
+
+  for (let key in wins) {
+    result.push({player: key, wins: wins?.[key]});
+  }
+  return result.sort((a, b) => (a.wins > b.wins ? -1 : 1));
+}
+
+function Scoreboard() {
+  const gameHistory = useGameStore(state => state.history);
+  const rankings = formatWins(gameHistory);
   return (
     <Screen>
-      <Text>Scoreboard Screen</Text>
+      <HeaderText>Scoreboard</HeaderText>
+      {rankings.length ? (
+        <Rankings {...{rankings}} />
+      ) : (
+        <View style={styles.emptyState}>
+          <Text style={styles.emptyStateText}>No Matches Played Yet...</Text>
+        </View>
+      )}
     </Screen>
+  );
+}
+
+interface RankingProps {
+  rankings: Ranking[];
+}
+
+function Rankings({rankings}: RankingProps) {
+  return (
+    <View>
+      <View style={styles.rankRow}>
+        <Text style={[styles.text, styles.rowHeader]}>Name</Text>
+        <Text style={[styles.text, styles.rowHeader]}>Wins</Text>
+      </View>
+      {rankings.map(rank => (
+        <RankRow key={rank.player} name={rank.player} wins={rank.wins} />
+      ))}
+    </View>
+  );
+}
+
+interface RankRowProps {
+  name: string;
+  wins: number;
+}
+
+function RankRow({name, wins}: RankRowProps) {
+  return (
+    <View style={styles.rankRow}>
+      <View style={styles.firstColumn}>
+        <Text style={styles.text}>{name}</Text>
+      </View>
+      <Text style={styles.text}>{wins}</Text>
+    </View>
   );
 }
 
--- src/screens/Scoreboard/styles.ts
+++ src/screens/Scoreboard/styles.ts
@@ -1,3 +1,33 @@
-import {StyleSheet} from 'react-native/types';
+import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+import {Color, FontWeight} from '../../theme';
+
+export default StyleSheet.create({
+  emptyState: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  emptyStateText: {
+    color: Color.LIGHT_GRAY,
+    fontSize: 20,
+  },
+  firstColumn: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 8,
+  },
+  rankRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: 50,
+    justifyContent: 'space-between',
+  },
+  rowHeader: {
+    color: Color.PRIMARY,
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: FontWeight.SEMI_BOLD,
+  },
+});
