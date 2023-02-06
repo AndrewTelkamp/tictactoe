1e5fced3abc61d269a0f07f75edd3d40b9176167
andrewtelkamp <andrew.telkamp@gmail.com>
2023-02-05 19:00:17 -0700

    dynamically setting height

--- src/components/GameSquare/index.tsx
+++ src/components/GameSquare/index.tsx
@@ -1,36 +1,67 @@
-import {memo} from 'react';
+import {memo, useState} from 'react';
 import {Pressable, StyleSheet, Text} from 'react-native';
 
+import {Color, FontWeight} from '../../theme';
+
 interface GameSquareProps {
+  hasRightBorder?: boolean;
+  hasBottomBorder?: boolean;
   onPress?: () => void;
   playerNumber?: number;
 }
 
-function GameSquare({onPress, playerNumber}: GameSquareProps) {
+function GameSquare({
+  hasRightBorder,
+  hasBottomBorder,
+  onPress,
+  playerNumber,
+}: GameSquareProps) {
+  const [height, setHeight] = useState(100);
+
   let text;
   if (playerNumber) {
     text = playerNumber === 1 ? 'X' : '0';
   }
 
+  function handleLayout(e: any) {
+    const {layout} = e.nativeEvent;
+    setHeight(Math.floor(layout.width));
+  }
+
   return (
-    <Pressable {...{onPress}} style={styles.square}>
+    <Pressable
+      {...{onPress}}
+      onLayout={handleLayout}
+      style={[
+        styles.square,
+        hasBottomBorder && styles.borderBottom,
+        hasRightBorder && styles.borderRight,
+        {height, width: height},
+      ]}>
       <Text style={styles.text}>{text}</Text>
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
+  borderBottom: {
+    borderBottomColor: Color.BLACK,
+  },
+  borderRight: {
+    borderRightColor: Color.BLACK,
+  },
   square: {
     alignItems: 'center',
-    borderColor: '#000',
+    borderColor: 'transparent',
+    borderLeftColor: Color.BLACK,
+    borderTopColor: Color.BLACK,
     borderWidth: 1,
-    justifyContent: 'center',
     flex: 1,
-    minHeight: 100,
+    justifyContent: 'center',
   },
   text: {
     fontSize: 60,
-    fontWeight: '700',
+    fontWeight: FontWeight.EXTRA_BOLD,
   },
 });
 
