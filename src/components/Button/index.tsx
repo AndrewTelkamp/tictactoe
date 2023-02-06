fadd0d8a9413bb5b4e9fc9310fc71ec75dc62f75
andrewtelkamp <andrew.telkamp@gmail.com>
2023-02-05 22:28:45 -0700

    cleanup icons

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,21 +6,12 @@ import styles from './styles';
 import type {PressableProps, ViewStyle} from 'react-native';
 
 export interface ButtonProps extends PressableProps {
-  leftIcon?: JSX.Element;
-  rightIcon?: JSX.Element;
   onPress: () => void;
   title: string;
   style?: ViewStyle;
 }
 
-function Button({
-  leftIcon,
-  onPress,
-  rightIcon,
-  style,
-  title,
-  ...props
-}: ButtonProps) {
+function Button({onPress, style, title, ...props}: ButtonProps) {
   return (
     <Pressable {...props} {...{onPress}} style={[styles.button, {...style}]}>
       <Text style={styles.text}>{title}</Text>
